d2a8e79384a7cd7fec63d4cb30155a9f4d6e55cc
ozawa-masaki-ambl <masaki.ozawa.kq@ambl.co.jp>
2023-09-05 15:26:35 +0900

    15

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -14,9 +14,18 @@ fun main(args: Array<String>) {
 		// Hello, world!
 	val name:String="Masa"
 	val num = 23
-
 	println("$name is $num years old")
-
+	val green = listOf(1,4,5)
+	val red = listOf(33,2)
+	val count = green.count()+red.count()
+	println(count)
+	val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
+	val requested = "ftp"
+	val isSupported = requested.uppercase()in SUPPORTED// Write your code here
+		println("Support for $requested: $isSupported")
+	val number2word = mapOf("1" to "A","2" to "B","3" to "C",)// Write your code here
+	val n = 2
+	println("$n is spelt as '${number2word["2"]}'")
 
 
 
